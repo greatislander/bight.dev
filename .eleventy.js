22450d57b8d69b144bccbaf12e8ee2c726425e7d
Ned Zimmerman <ned@bight.dev>
2020-01-16 11:45:51 -0700

    feat: add Keybase proof

--- .eleventy.js
+++ .eleventy.js
@@ -29,6 +29,7 @@ module.exports = function(config) {
 	// Passthrough copy
 	config.addPassthroughCopy("src/fonts");
 	config.addPassthroughCopy("src/images");
+	config.addPassthroughCopy("src/.well-known/keybase.txt");
 	config.addPassthroughCopy("src/admin/config.yml");
 	config.addPassthroughCopy("src/admin/previews.js");
 	config.addPassthroughCopy("node_modules/nunjucks/browser/nunjucks-slim.js");
